06064f29b45f8cb52f3b72af93a515108adda454
Evandro Taroco <evandrotaroco@hotmail.com>
2025-03-25 23:38:59 -0300

    Corrigindo erros iniciais

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+//importa sua rota aqui
 
 const host = "0.0.0.0";
 const porta = 4000;
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cors({
@@ -13,7 +16,7 @@ app.use(cors({
     "Access-Control-Allow-Origin":"*"
 }));
 
-dotenv.config();
+//app.use('/exemplo', rotaExemplo);
 
 app.listen(porta, host, () => {
     console.log(`Servidor escutando em http://${host}:${porta}`);
