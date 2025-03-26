7c8ff5989c61071a1e5c211f5d45c90ea33705f2
MatheusChizzolini <matchizzolini@gmail.com>
2025-03-26 10:04:37 -0300

    Terminando o CRUD de usuário (backend).

--- model/usuario.model.js
+++ model/usuario.model.js
@@ -1,3 +1,5 @@
+import UsuarioPersistence from "../persistence/usuario.persistence.js";
+
 export default class Usuario {
     #email;
     #senha;
@@ -31,5 +33,45 @@ export default class Usuario {
 
     set privilegio(novoPrivilegio) {
         this.#privilegio = novoPrivilegio;
+    }
+
+    validarUsuario(usuario) {
+        if (usuario instanceof Usuario) {
+            if (usuario.email != "" && usuario.senha != "" && usuario.privilegio != "") {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+
+    validarEmail(usuario) {
+        if (usuario instanceof Usuario) {
+            if (usuario.email != "") {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+
+    async incluir(conexao, usuario) {
+        const usuarioPersist = new UsuarioPersistence();
+        return await usuarioPersist.incluir(conexao, usuario);
+    }
+
+    async editar(conexao, usuario) {
+        const usuarioPersist = new UsuarioPersistence();
+        return await usuarioPersist.alterar(conexao, usuario);
+    }
+
+    async excluir(conexao, usuario) {
+        const usuarioPersist = new UsuarioPersistence();
+        return await usuarioPersist.excluir(conexao, usuario);
+    }
+
+    async consultar(conexao, termo) {
+        const usuarioPersist = new UsuarioPersistence();
+        return await usuarioPersist.consultar(conexao, termo);
     }
 }
--- persistence/usuario.persistence.js
+++ persistence/usuario.persistence.js
@@ -1,9 +1,64 @@
 import Usuario from "../model/usuario.model.js";
 
-export default class UsuarioDAO {
+export default class UsuarioPersistence {
     constructor() {
         this.init();
     }
 
+    async incluir(conexao, usuario) {
+        if (usuario instanceof Usuario) {
+            const sql = `INSERT INTO usuario(email, senha, privilegio) values (?, ?, ?)`;
+
+            let parametros = [
+                usuario.email,
+                usuario.senha,
+                usuario.privilegio
+            ];
+
+            await conexao.execute(sql, parametros);
+            await conexao.release();
+        }
+    }
+
+    async alterar(conexao, usuario) {
+        if (usuario instanceof Usuario) {
+            const sql = `UPDATE usuario SET email = ?, senha = ?, privilegio = ?`;
+
+            let parametros = [
+                usuario.email,
+                usuario.senha,
+                usuario.privilegio
+            ];
+
+            await conexao.execute(sql, parametros);
+            await conexao.release();
+        }
+    }
     
+    async excluir(conexao, usuario) {
+        if (usuario instanceof Usuario) {
+            const sql = `DELETE FROM usuario WHERE email = ?`;
+
+            let parametros = [
+                usuario.email
+            ];
+
+            await conexao.execute(sql, parametros);
+            await conexao.release();
+        }
+    }
+
+    async consultar(conexao, termo) {
+        let sql = "SELECT * FROM usuario";
+        let parametros = [];
+
+        if (termo) {
+            sql = ` WHERE descricao LIKE ?`;
+            parametros = [termo];
+        }
+
+        const [dataBase, campos] = await conexao.execute(sql, parametros);
+        await conexao.release();
+        return dataBase;
+    }
 }
