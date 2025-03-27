41cb60b8528155939fcfc3dda5c33aeecc13ecab
Matheus Chizzolini <95874868+MatheusChizzolini@users.noreply.github.com>
2025-03-27 02:09:46 -0300

    Add files via upload

---
+++ model/apoiador.model.js
@@ -0,0 +1,68 @@
+import ApoiadorDAO from "../persistence/apoiador.persistence.js";
+
+export default class Apoiador {
+    #idApoiador;
+    #cpf;
+    #nome;
+    #email;
+    #endereco;
+    #telefone;
+
+    constructor(idApoiador = 0, cpf = "", nome = "", email = "", endereco = "", telefone = "") {
+        this.#idApoiador = idApoiador;
+        this.#cpf = cpf;
+        this.#nome = nome;
+        this.#email = email;
+        this.#endereco = endereco;
+        this.#telefone = telefone;
+    }
+
+    // Getters e Setters
+    get idApoiador() { return this.#idApoiador; }
+    set idApoiador(value) { this.#idApoiador = value; }
+
+    get cpf() { return this.#cpf; }
+    set cpf(novoCpf) { this.#cpf = novoCpf; }
+
+    get nome() { return this.#nome; }
+    set nome(novoNome) { this.#nome = novoNome; }
+
+    get email() { return this.#email; }
+    set email(novoEmail) { this.#email = novoEmail; }
+
+    get endereco() { return this.#endereco; }
+    set endereco(novoEndereco) { this.#endereco = novoEndereco; }
+
+    get telefone() { return this.#telefone; }
+    set telefone(novoTelefone) { this.#telefone = novoTelefone; }
+
+    // Métodos Assíncronos
+    async incluir(conexao) {
+        const apoiadorDAO = new ApoiadorDAO();
+        return await apoiadorDAO.incluir(conexao, this);
+    }
+
+    async editar(conexao) {
+        const apoiadorDAO = new ApoiadorDAO();
+        return await apoiadorDAO.editar(conexao, this);
+    }
+
+    async excluir(conexao) {
+        const apoiadorDAO = new ApoiadorDAO();
+        return await apoiadorDAO.excluir(conexao, this);
+    }
+
+    async consultar(conexao, termo) {
+        const apoiadorDAO = new ApoiadorDAO();
+        return await apoiadorDAO.consultar(conexao, termo);
+    }
+
+    // Validações
+    validarApoiador() {
+        return this.#email !== "" && this.#cpf !== "";
+    }
+
+    validarIdApoiador() {
+        return this.#idApoiador > 0;
+    }
+}
