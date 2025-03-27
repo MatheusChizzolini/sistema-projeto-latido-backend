2cb48d8437a44c7d04114c2d31e8b6fcda4d2b64
Matheus Chizzolini <95874868+MatheusChizzolini@users.noreply.github.com>
2025-03-27 02:10:13 -0300

    Add files via upload

---
+++ routes/apoiador.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import ApoiadorControl from "../control/apoiador.control.js";
+
+const apoiadorControl = new ApoiadorControl();
+const rotaApoiador = Router();
+
+rotaApoiador.post("/", apoiadorControl.gravar);
+rotaApoiador.put("/:idApoiador", apoiadorControl.editar);
+rotaApoiador.patch("/:idApoiador", apoiadorControl.editar);
+rotaApoiador.delete("/:idApoiador", apoiadorControl.excluir);
+rotaApoiador.get("/:idApoiador", apoiadorControl.consultar);
+rotaApoiador.get("/", apoiadorControl.consultar);
+
+export default rotaApoiador;
